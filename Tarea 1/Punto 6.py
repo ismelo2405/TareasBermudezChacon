766d9d188baf55fc4a75bcc7ed9c20ffa3875ca7
Bernardo Chacon Montero <bchaconm005@gmail.com>
2025-08-14 17:19:03 -0600

    Arreglados los errores del paso 6

--- Tarea 1/Punto 6.py
+++ Tarea 1/Punto 6.py
@@ -1,3 +1,5 @@
 
 
-def add(a,b): print("Sum is:", a+b) # 3 violaciones presentes en este codigo: #E231 #E261 #E501
+def add(a, b): print("Sum is:", a+b)
+# 3 violaciones presentes en este codigo:
+# #E231 #E261 #E501
